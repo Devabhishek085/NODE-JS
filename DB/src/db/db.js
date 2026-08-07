421575c0ae0374753594bf2792286f49197572e4
Abhishek Pal <palabhishek085@gmail.com>
2026-08-07 23:44:21 +0530

    CRUD operations uisng the DB connection

--- DB/src/db/db.js
+++ DB/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 async function connectDB(){
-    await mongoose.connect("mongodb+srv://Abhishek:4KFFlMW8z0t8WMlk@cluster0.ta6pzcq.mongodb.net/halley")
+    await mongoose.connect("")
 
     console.log("Database connected successfully");
     
